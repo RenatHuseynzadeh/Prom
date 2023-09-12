901ad7adaaf5ebfb15f550c5e7e5511096142186
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-09-12 19:06:39 +0300

    Add files via upload

---
+++ PromiseDz.js
@@ -0,0 +1,39 @@
+// 1
+// Это как бы функция которую мы передаём другой функци что бы она выполнела её потом
+
+
+// 2
+
+
+//  function alertInfo(name, age) {
+    //  console.log(`Привет я ${name} и мне ${age} лет`);
+//  }
+//  
+//  function userInfo() {
+    //  let name = 'Alex';
+    //  let age = 19;
+    //  alertInfo(name, age)
+//  }
+//  
+//  userInfo()
+
+// 3
+// 2-я т.к мы пишем её внутри первой функции
+
+// 4
+// function startProgram(showMessage) {
+    // let a = 'Запуск'
+    // showMessage(a)
+    // 
+// }
+// 
+// startProgram((result) => {
+    // console.log(result);
+// })
+
+
+// 5
+// это обещяние,он обещяет что-то будет
+
+// 10
+// это когда несколько действий делается одновременно 
